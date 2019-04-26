8533e9dfa8b7057cbf3780d4be6d16c3eb9dad7d
Erie M. Adames <eriem.adames@Eries-Mac.local>
2019-04-26 09:00:53 -0700

    nit

---
+++ Dust2.0/Dust2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dust2.0
+//
+//  Created by Erie M. Adames on 4/26/19.
+//  Copyright © 2019 Erie M. Adames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
